dbfbbfb72d425f0dc5f712ae56767bf26c221dd9
Noel Hoppe <noel.hoppe@gmx.de>
2025-06-22 15:37:07 +0200

    rewriting sqlalchemy orm models version 2.0 conform

--- backend/src/models/todo.py
+++ backend/src/models/todo.py
@@ -1,11 +1,26 @@
+# --- EXTERN IMPORTS ---
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
 
+# --- INTERN IMPORTS ---
 from src.core import db
 
 class ToDo(db.Base):
   __tablename__ = "todos"
-  id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-  title = Column(String,  nullable=False)
-  due_to = Column(DateTime, nullable=True)
+  
+  # id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+  id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+  # title = Column(String,  nullable=False)
+  title: Mapped[str]
+
+  # due_to = Column(DateTime, nullable=True)
+  due_to: Mapped[Optional[DateTime]] 
+
   is_done = Column(Boolean, default=False)
-  user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+  is_done: Mapped[bool] = mapped_column(default=False)
+
+
+  # user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+  user_id: Mapped[int] = mapped_column(ForeignKey("users.id")) 
--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,9 +1,18 @@
+# --- EXTERN IMPORTS ---
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
+# --- INTERN IMPORTS ---
 from src.core import db
 
 class User(db.Base):
   __tablename__ = "users"
-  id = Column(Integer, primary_key=True, nullable=False)
-  username = Column(String, unique=True)
-  hashed_password = Column(String)
+
+  # id = Column(Integer, primary_key=True, nullable=False)
+  id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+  # username = Column(String, unique=True)
+  username: Mapped[str] = mapped_column(unique=True)
+
+  # hashed_password = Column(String)
+  hashed_password: Mapped[str] 
